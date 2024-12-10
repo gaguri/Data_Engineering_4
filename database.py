a5c102f8270522231e1c3d12d046953326aa7bc6
Svetlana Levina <gurigaguri@yandex.com>
2024-12-11 04:44:36 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import sqlite3
 
 def connect_to_database():
-    connect = sqlite3.connect(r'49\all_tasks.db')
+    connect = sqlite3.connect('all_tasks.db')
     connect.row_factory = sqlite3.Row
     return connect
